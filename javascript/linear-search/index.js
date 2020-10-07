654a92a3bbac94ab5cea0455efc83437379f34f0
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-10-07 09:57:44 -0300

    feat: create main file of linear search and calls linear search function

---
+++ javascript/linear-search/index.js
@@ -0,0 +1,10 @@
+const linearSearch =  require('./linearSearch')
+
+let arr = [ 2, 3, 4, 10, 40 ];
+
+console.log(linearSearch(arr,2))
+console.log(linearSearch(arr,3))
+console.log(linearSearch(arr,4))
+console.log(linearSearch(arr,5))
+console.log(linearSearch(arr,10))
+console.log(linearSearch(arr,40))
