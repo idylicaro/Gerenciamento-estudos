9f7e585ad6b186da04c1bc56bd56441285a5e59f
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-10-07 09:49:04 -0300

    feat: create main file of factorial and calls factorial method

---
+++ java/factorial/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(Factorial.factorial(3));
+        System.out.println(Factorial.factorial(4));
+        System.out.println(Factorial.factorial(5));
+        System.out.println(Factorial.factorial(6));
+    }
+}
